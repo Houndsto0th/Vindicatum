0a1801455888adafad8d10ecdb7f0f33c3372469
Houndsto0th <bry.kinnear@gmail.com>
2015-08-19 17:39:58 -0600

    updated roster

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,23 +1,24 @@
 class PagesController < ApplicationController
   TANKS = [
     {name: "Val", class: "Paladin"},
+    {name: "Syrynity", class: "Monk"}
   ]
 
   DPS = [
     {name: "Paleaura", class: "DK"},
     {name: "Pudders", class: "Druid"},
-    {name: "Ilzan", class: "Rogue"},
+    {name: "Curandeiro", class: "Shaman"},
     {name: "Ippon", class: "Rogue"},
     {name: "Xanatu", class: "Rogue"},
     {name: "Candese", class: "Paladin"},
-    {name: "Powerwaddle", class: "Paladin"},
+    {name: "Hyperwaddle", class: "Warrior"},
     {name: "Scootie", class: "Shaman"},
     {name: "Gronkle", class: "Warrior"},
     {name: "Kinoble", class: "Druid"},
     {name: "Arrow", class: "Hunter"},
     {name: "Tanned", class: "Priest"},
     {name: "Cryptrot", class: "Warlock"},
-    
+    {name: "Phosphos", class: "Mage"}
   ]
   
   HEALERS = [
